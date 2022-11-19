a3770453c112bbd103fd6b04b352412dc3c4f528
cesar <crivas@gmail.com>
2022-11-19 15:48:21 +0100

    changes

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import { express } from "express";
+import usuarioRoutes from './routes/usuarioRoutes.js'
 
 // crear la app
 const app = express()
+
+// Routing
+app.use('/', usuarioRoutes)
+
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log('servidor prueba')
+})
